1d7c2cebf213553f88c4d309b1688895dd05f69e
Sergey Shcherbina <bshbfg@gmail.com>
2021-12-12 23:56:47 +0500

    added events filter

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,25 @@
+import { Fragment } from "react";
+
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
+import { useRouter } from "next/router";
 
 function AllEventsPage() {
   const events = getAllEvents();
+  const router = useRouter();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
 
   return (
-    <div>
+    <Fragment>
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
-    </div>
+    </Fragment>
   );
 }
 
