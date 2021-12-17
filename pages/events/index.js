a19eb8dcfe550d8f9deeb7777fa405becea7c42d
Sergey Shcherbina <bshbfg@gmail.com>
2021-12-17 18:34:20 +0500

    added ssg for on the all events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,13 @@
 import { Fragment } from "react";
 
-import { getAllEvents } from "../../dummy-data";
+import { getAllEvents } from "../../halpers/api-util";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
 import { useRouter } from "next/router";
 
-function AllEventsPage() {
-  const events = getAllEvents();
+function AllEventsPage(props) {
   const router = useRouter();
+  const { events } = props;
 
   function findEventsHandler(year, month) {
     const fullPath = `/events/${year}/${month}`;
@@ -21,6 +21,16 @@ function AllEventsPage() {
       <EventList items={events} />
     </Fragment>
   );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events,
+    },
+  };
 }
 
 export default AllEventsPage;
